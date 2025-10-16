5603108fb3f56592636370505ac8801cd145510f
Brian <72092838+BrianWalczak@users.noreply.github.com>
2025-10-16 13:32:57 -0500

    Create audio.js

---
+++ app/libs/audio.js
@@ -0,0 +1,15 @@
+class StreamAudio {
+    constructor(display, callback) {
+        // still working on it!
+    }
+
+    async start(display) {
+        // still working on it!
+    }
+
+    stop() {
+        // still working on it!
+    }
+}
+
+module.exports = StreamAudio;
